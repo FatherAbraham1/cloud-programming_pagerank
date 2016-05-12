41389d716f94863cb440e85162b555507e48c845
Cheng-Yu Hsu <m@cyhsu.me>
2016-05-12 20:46:03 +0800

    add data processor

---
+++ hadoop-mr/src/main/java/cp2016/pagerank/parse/App.java
@@ -0,0 +1,33 @@
+package cp2016.pagerank.parse;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class App {
+    public static void main( String[] args ) throws IllegalArgumentException, IOException, ClassNotFoundException, InterruptedException {
+    	Configuration config = new Configuration();
+    	config.set("mapreduce.output.textoutputformat.separator", "\t");
+    	
+		Job job = Job.getInstance(config, "PageLinkBuilder");
+		job.setJarByClass(App.class);
+
+		job.setMapperClass(RowMapper.class);
+		
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
---
+++ hadoop-mr/src/main/java/cp2016/pagerank/parse/RowMapper.java
@@ -0,0 +1,81 @@
+package cp2016.pagerank.parse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang3.text.WordUtils;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import com.alibaba.fastjson.JSON;
+
+public class RowMapper extends Mapper<LongWritable, Text, Text, Text> {
+	
+	private final Pattern titlePattern = Pattern.compile("<title>.+</title>");
+	private final Pattern textPattern = Pattern.compile("<text>.+</text>");
+	private final Pattern linkPattern = Pattern.compile("\\[\\[.+\\]\\]");
+	
+	public void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+		String[] vals = value.toString().split("\n");
+		for (String val : vals) {
+			if (val == null || val.isEmpty()) {
+				continue;
+			} else {
+				String title = null;
+				Matcher titleMatcher = titlePattern.matcher(val);
+				if (titleMatcher.find()) {
+					title = titleMatcher.group();
+					title = title.substring(8, title.length() - 8);
+					title = StringEscapeUtils.unescapeXml(title);
+					title = WordUtils.capitalize(title);
+				}
+				
+				String text = null;
+				Matcher textMatcher = textPattern.matcher(val);
+				if (textMatcher.find()) {
+					text = textMatcher.group();
+					text = text.substring(7, text.length() - 7);
+				}
+				
+				if (title == null || text == null) {
+					continue;
+				}
+				
+				List<String> links = parseLinks(text);
+				context.write(new Text(title), new Text(JSON.toJSONString(links)));
+			}
+		}
+	}
+	
+	private List<String> parseLinks(String content) {
+		List<String> links = new ArrayList<>();
+		Matcher linkMatcher = linkPattern.matcher(content);
+		while (linkMatcher.find()) {
+			String linkContent = linkMatcher.group();
+			linkContent = linkContent.substring(2, linkContent.length() - 2);
+			{
+				String[] tmp = linkContent.split("\\|");
+				if (tmp.length > 0) {
+					linkContent = tmp[0];
+				}
+			}
+			
+			{
+				String[] tmp = linkContent.split("#");
+				if (tmp.length > 0) {
+					linkContent = tmp[0];
+				}
+			}
+			
+			links.add(WordUtils.capitalize(StringEscapeUtils.unescapeXml(linkContent)));
+			
+		}
+		return links;
+	}
+}
