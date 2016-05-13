9457680d3953239ad1cbc514c47e788ec16a8c62
Cheng-Yu Hsu <m@cyhsu.me>
2016-05-13 14:43:00 +0800

    GG

--- hadoop-mr/src/main/java/cp2016/pagerank/parse/RowMapper.java
+++ hadoop-mr/src/main/java/cp2016/pagerank/parse/RowMapper.java
@@ -22,6 +22,7 @@ public class RowMapper extends Mapper<LongWritable, Text, Text, Text> {
 	
 	public void map(LongWritable key, Text value, Context context)
 			throws IOException, InterruptedException {
+		System.out.println(value.toString());
 		String[] vals = value.toString().split("\n");
 		for (String val : vals) {
 			if (val == null || val.isEmpty()) {
