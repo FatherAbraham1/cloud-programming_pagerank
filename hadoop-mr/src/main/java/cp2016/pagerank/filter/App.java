66f0fa1c7b8efebacfacf04f3db32ee08294f68b
Cheng-Yu Hsu <m@cyhsu.me>
2016-05-14 07:43:25 +0800

    ..

--- hadoop-mr/src/main/java/cp2016/pagerank/filter/App.java
+++ hadoop-mr/src/main/java/cp2016/pagerank/filter/App.java
@@ -11,7 +11,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import cp2016.pagerank.common.TitleLinkPair;
@@ -37,7 +37,7 @@ public class App {
 
 		job.setNumReduceTasks(0);
 		
-		TextInputFormat.addInputPath(job, new Path(args[0]));
+		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
