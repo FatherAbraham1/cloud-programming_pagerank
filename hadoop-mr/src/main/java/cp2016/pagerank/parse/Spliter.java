ddb93c743f2889a7708f454a046347bee500aca7
Cheng-Yu Hsu <m@cyhsu.me>
2016-05-14 09:46:30 +0800

    ..

--- hadoop-mr/src/main/java/cp2016/pagerank/parse/Spliter.java
+++ hadoop-mr/src/main/java/cp2016/pagerank/parse/Spliter.java
@@ -3,13 +3,14 @@ package cp2016.pagerank.parse;
 import java.util.Random;
 
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
-public class Spliter extends Partitioner<IntWritable, Text> {
+import cp2016.pagerank.common.TitleLinkPair;
+
+public class Spliter extends Partitioner<IntWritable, TitleLinkPair> {
 
 	@Override
-	public int getPartition(IntWritable key, Text value, int numReducers) {
+	public int getPartition(IntWritable key, TitleLinkPair value, int numReducers) {
 		Random gen = new Random();
 		return gen.nextInt(numReducers);
 	}
