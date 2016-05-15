761989a6995bafb7d1ba752fff9bca08aa172468
Cheng-Yu Hsu <m@cyhsu.me>
2016-05-15 21:19:30 +0800

    ..

--- spark/src/main/scala/cp2016/pagerank/PageRank.scala
+++ spark/src/main/scala/cp2016/pagerank/PageRank.scala
@@ -24,7 +24,7 @@ object PageRank {
     }
 
     val pages = ctx.textFile(inputPath, ctx.defaultParallelism)
-    
+
     val linkPattern = """\[\[[^\]]+\]\]""".r
     val linkSplitPattern = "[#|]"
     var adjMatrix = pages.flatMap { line =>
@@ -36,19 +36,19 @@ object PageRank {
                              .map { link => link.substring(2, link.length() - 2).split(linkSplitPattern) }
                              .filter { arr => arr.size > 0 }
                              .map { arr => (title, arr(0)) }
-                             
+
       links.union(List((title, "")))
     }
-    
-    val numDocs = adjMatrix.map(_._1).distinct().map(_ => 1).sum() 
+
+    val numDocs = adjMatrix.map(_._1).distinct().map(_ => 1).sum()
     val teleport = 0.15 * (1.0 / numDocs)
-    
+
     adjMatrix = adjMatrix.map(x => (x._2, x._1))
                          .leftOuterJoin(adjMatrix, ctx.defaultParallelism * 12)
                          .filter(x => x._1 == "" || !x._2._2.isEmpty)
                          .filter(x => !x._2._2.exists(e => e == ""))
                          .map(x => (x._2._1, x._1))
-                      
+
     val adjMat = adjMatrix.groupByKey().cache()
     var ranks = adjMat.map(x => (x._1, 1.0 / numDocs))
 
@@ -60,32 +60,32 @@ object PageRank {
                                   .filter(tup => tup._2._1.size == 1)
                                   .map(tup => tup._2._2)
                                   .sum()
-                                  
+
       sinkNodeRankSum = sinkNodeRankSum / numDocs * 0.85
-    
-      
+
+
       val updates = adjMat.join(ranks)
                           .values
                           .filter(tup => tup._1.size > 1)
                           .flatMap { case (links, rank) =>
                             val size = links.size
                             links.filter(x => x != "")
-                                 .map(x => (x, rank / size))
+                                 .map(x => (x, rank / size - 1))
                            }
       var newRanks = updates.reduceByKey(_ + _)
       newRanks = ranks.fullOuterJoin(newRanks).map(x => (x._1, x._2._2.getOrElse(0.0) * 0.85 + teleport + sinkNodeRankSum))
       diff = ranks.join(newRanks).map(x => math.abs(x._2._1 - x._2._2)).sum()
       println("diff = " + diff.toString())
       ranks = newRanks
-      
+
       val end = System.nanoTime()
       println(end - begin)
     } while(diff >= 0.001)
-    
+
     ranks.sortBy(tup => (-tup._2, tup._1), true, ctx.defaultParallelism * 12)
           .map(tup => tup._1 + "\t" + tup._2.toString())
           .saveAsTextFile(outputDir)
-          
+
     try { ctx.stop } catch { case _ : Throwable => {} }
   }
 }
